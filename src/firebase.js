a99fa114d8c793201dbb4476943a646b9be2d405
extratha <thakorntongrak@gmail.com>
2025-03-22 21:43:18 +0700

    change domain

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, addDoc, updateDoc,deleteDoc, doc,onSnapshot } from 'firebase/firestore';
+import { getFirestore, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, onSnapshot } from 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCFAsHVcgDU_jIskVc2SskSo4ufKokeL_g",
-    authDomain: "unique-number-931d0.firebaseapp.com",
-    projectId: "unique-number-931d0",
-    storageBucket: "unique-number-931d0.firebasestorage.app",
-    messagingSenderId: "769264750690",
-    appId: "1:769264750690:web:36d2679b136b5801da813b",
-    measurementId: "G-TLR9Z95093"
+    apiKey: "AIzaSyAjq0jaRQ8pQfak-zXNf0jXgF5x1g4rqc0",
+    authDomain: "fake-ito-boardgame-th.firebaseapp.com",
+    projectId: "fake-ito-boardgame-th",
+    storageBucket: "fake-ito-boardgame-th.firebasestorage.app",
+    messagingSenderId: "36323179426",
+    appId: "1:36323179426:web:a88809a21713dd7612d0fb",
+    measurementId: "G-7RJK9P89VY"
 };
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-export { db, collection, getDocs, addDoc, updateDoc,deleteDoc, doc,onSnapshot}
+export { db, collection, getDocs, addDoc, updateDoc, deleteDoc, doc, onSnapshot }
